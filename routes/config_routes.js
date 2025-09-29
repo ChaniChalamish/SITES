b48534bc2a83c02de751d232bfee598757c096e8
ChaniChalamish <b0527626958@gmail.com>
2025-09-29 15:14:33 +0300

    fixed missing ref

--- routes/config_routes.js
+++ routes/config_routes.js
@@ -1,7 +1,9 @@
 const sitesRouter = require("./sites");
 const usersRouter = require("./users");
+const booksRouter = require("./books")
 
 exports.routesInit = (app) => {
   app.use("/sites", sitesRouter);
   app.use("/users", usersRouter);
+  app.use("/books",booksRouter);
 };
